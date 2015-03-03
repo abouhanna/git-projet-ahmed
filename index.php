244063ebeeb1c1eb6fc86091a6d3181bb95cb0cb
Ahmed BOUHANNA <abouhanna@cdta.dz>
2015-03-03 10:32:44 +0100

    pull request accepté

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
-<?php echo "hello"; 
-function test()
-{echo("modif kbelgacem");
-}?>
+<?php 
+
+	echo "hello"; 
+
+
+	//appelle de la fonction test
+	echo test();
+
+	
+	//declaration de la fonction test
+	function test()
+	{	
+		echo("modif kbelgacem");
+	}
+
+
+?>
